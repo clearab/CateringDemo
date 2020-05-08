6a43f83d628c98641b7be1fc2912af7dab0e8794
Jeff Derstadt <Jeffders@users.noreply.github.com>
2020-05-08 11:48:59 -0700

    Jeffders/cosmos fix (#19)
    * refresh
    
    * Update aaf and fix dispose

--- Catering/CateringDb.cs
+++ Catering/CateringDb.cs
@@ -12,7 +12,7 @@ using Model = Catering.Models;
 
 namespace Catering
 {
-    public class CateringDb
+    public class CateringDb : IDisposable
     {
         private string _endpointUri;
         private string _primaryKey;
@@ -126,5 +126,41 @@ namespace Catering
             ItemResponse<Model.User> response = await this._container.Value.UpsertItemAsync(user, new PartitionKey(PartitionKeyValue));
             return response.Resource;
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _cosmosClient.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~CateringDb()
+        // {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
